1f34fc7e55c1987550fbc5d70ed5611a343b101d
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-02-28 22:01:40 -0300

    heroku porta

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+  console.log(`Rodando na porta ${PORT}`)
+})
