5f1ef2ccc739c236ae3a8887130fe63b1d635a6a
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-05-21 13:11:28 -0300

    build(connection): dont env variaveis

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # backend-phonebook
+
+(backend)[https://aqueous-hollows-23591.herokuapp.com/]
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.use(express.json());
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Rodando na porta ${PORT}`)
-})
+});
