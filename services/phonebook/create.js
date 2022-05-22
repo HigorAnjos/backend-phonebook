9e817d040d52df302462d0a8cd9f896decc22a8e
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-05-21 22:58:44 -0300

    build(services): criando a camanda ser servicos

---
+++ services/phonebook/create.js
@@ -0,0 +1,8 @@
+const model = require('../../models/phonebook');
+
+const create = async (name, phone, use_id) => {
+  const phonebook = await model.create(name, phone, use_id);
+  return phonebook;
+}
+
+module.exports = create;
---
+++ services/phonebook/index.js
@@ -0,0 +1,7 @@
+const list = require('./list');
+const create = require('./create');
+const find = require('./find');
+const remove = require('./remove');
+const update = require('./update');
+
+module.exports = { list, create, find, remove, update };
