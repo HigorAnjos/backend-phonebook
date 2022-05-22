ec321a4be38428f137aff4a7d6fc934d0babc030
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-05-22 12:52:36 -0300

    feat(test): testando a parte de listar

--- tests/models/phonebook/create.test.js
+++ tests/models/phonebook/create.test.js
@@ -1,9 +1,41 @@
-decribet(('Adicionar um novo numero ao BD user ID 1'), async () => {
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../models/connection');
+const { execute } = require('../../../models/connection');
+const models = requie('../../models/phonebook/index');
 
-  it ('Deve retornar um objeto com o ID do novo numero', async () => {
-    const phonebook = await phonebookCreate('Teste', '123456789', 1);
+decribet(('Listar numeros do user ID 1'), async () => {
+  const DB_RESP = {
+    id: 1,
+    phonebook:
+    [
+        {
+            "name": "alguem",
+            "number": "+380967891234"
+        },
+    ]
+  }
 
-    expect(phonebook).toHaveProperty('id');
+  before(() => {
+    sinon.stub('connection', execute).resolves(DB_RESP);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
+  it ('Deve retornar um objeto com a chave id igual a 1', async () => {
+    const user_id = 1;
+    const expected = DB_RESP.id;
+    const result = await models.list(user_id);
+    expect(result.id).toEqual(expected);
+  });
+
+  it ('Deve retornar um objeto com a chave phonebook igual a [{name: "alguem", number: "+380967891234"}]', async () => {
+    const user_id = 1;
+    const expected = DB_RESP.phonebook;
+    const result = await models.list(user_id);
+    expect(result.phonebook).toEqual(expected);
   }
 
 })
