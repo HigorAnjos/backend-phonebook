6b1870c08e9613952cef774c1b2ef3132cac5e4c
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-05-24 09:58:17 -0300

    feat(test): auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,23 +6,21 @@ const API_SECRET = process.env.SECRET_KEY || 'mysecret';
 const auth = (req, res, next) => {
   try {
     const { authorization } = req.headers;
-
+    
     if(!authorization) {
       return res.status(401).json({
         error: 'Token não informado'
       });
     }
-
+    
     try {
-      console.log('bug')
       const decoded = jwt.verify(authorization, API_SECRET);
-      console.log(decoded);
 
       const userName = decoded.data.name;
       const { id } = decoded.data;
-  
+      
       req.user = { id, userName } ;
-
+      
     } catch (err) {
       return res.status(400).json({
         message: 'Token inválido',
