3933b99400d3da504438e5a5a938e18eae20fa6e
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-05-23 23:47:56 -0300

    build(package): supertest dependencia para testar controller

---
+++ tests/controller/phonebook/list.test.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const controller = require('../../../controller/router')
+
+describe('Destando a rota phonebook/list', () => {
+  it('', () => {})
+});
