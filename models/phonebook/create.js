87c55633fb0069a7ddc3d9221c7e25317e77e5e9
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-03 09:31:29 -0300

    refactor(create list): :recycle: renomeando variaveis

--- models/phonebook/create.js
+++ models/phonebook/create.js
@@ -1,12 +1,14 @@
 const connection = require('../connection');
 
-const create = async (name, phone, use_id) => {
+const create = async (name, phone, userId) => {
   const query = 'INSERT INTO model_phonebook.phonebook (name, phone, use_id) VALUES (?, ?, ?)';
 
-  const [ phonebook ] = await connection.execute(query, [name, phone, use_id]);
+  const [ phonebook ] = await connection.execute(query, [name, phone, userId]);
   
   console.log("Aposta cadatro", phonebook);
   return phonebook;
 }
+
+create('Higor', '99999999', 1).then(console.log);
 
 module.exports = create;
--- models/phonebook/list.js
+++ models/phonebook/list.js
@@ -1,20 +1,22 @@
 const connection = require('../connection');
 
-const factory = (phonebook, user_id) => {
+const serialize = (phonebook, user_id) => {
 	return {
 		id: user_id,
 		phonebook, /// [{name: 'alguem', number: '+380967891234'}, {}, {}]
 	}
 }
 
-const list = async (user_id) => {
+const list = async (userId) => {
 	const query = `SELECT phone.name, phone.number
 	FROM model_phonebook.phonebook as phone
 	INNER JOIN model_phonebook.user as user ON user.id = phone.user_id
 	WHERE phone.user_id = ?`;
 
-	const [ phonebook ] = await connection.execute(query, [user_id]);
-  return factory(phonebook, user_id);
+	const [ phonebook ] = await connection.execute(query, [userId]);
+  return serialize(phonebook, userId);
 }
+
+list(1).then(console.log);
 
 module.exports = list;
