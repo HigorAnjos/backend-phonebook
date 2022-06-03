8b6dd25d3d0f62d6a8e335029e4c6decbc999934
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-03 14:08:17 -0300

    feat(update): :sparkles: atualizar contato

---
+++ controller/phonebook/update.js
@@ -0,0 +1,17 @@
+const services = require('../../services/phonebook/index');
+
+const update = async (req, res) => {
+  const { id: phoneId } = req.params;
+  const { name, phone } = req.body;
+  const { id: userId } = req.user;
+
+  const isOk = await services.update(userId, name, phone, phoneId);
+
+  if (!isOk) {
+    return res.status(400).json({ error: 'Não foi possivel atualizar o numero' });
+  }
+
+  return res.status(200).json({ message: 'Numero atualizado com sucesso' });
+}
+
+module.exports = update;
