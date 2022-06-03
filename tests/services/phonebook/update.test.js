f5e8cbd0522c4255e7a9334d553f1f172da6bd0b
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-03 19:34:46 -0300

    test(services): :white_check_mark: update

---
+++ tests/services/phonebook/update.test.js
@@ -0,0 +1,27 @@
+const models = require('../../../models/phonebook')
+const services = require('../../../services/phonebook')
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+describe('Testando services update quando sucesso', () => {
+  const affectedRows = 1;
+  before(() => {
+    sinon.stub(models, 'update').resolves(affectedRows);
+  })
+
+  after(() => {
+    models.update.restore();
+  });
+
+  it('Deve retornar numero de linha afetadas no banco', async () => {
+    const userId = 1;
+    const name = 'Nome';
+    const phone = '+38094654651';
+    const phoneId = 1;
+
+    const result = await services.update(userId, name, phone, phoneId);
+
+    expect(result).to.be.equal(1);
+    expect(result).to.be.a('number');
+  });
+});
