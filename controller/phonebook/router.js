e0fff2195bf4bb927df4cbc1fdcc3ad34ca50456
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-03 14:14:39 -0300

    feat(router): :sparkles: rotas crud

--- controller/phonebook/router.js
+++ controller/phonebook/router.js
@@ -2,10 +2,17 @@ const express = require('express');
 const auth = require('../../middlewares/auth');
 const list = require('./list');
 const create = require('./create');
+const find = require('./find');
+const update = require('./update');
+const remove = require('./remove');
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', auth, list);
-router.get('/create', auth, create);
+router.post('/create', auth, create);
+router.get('/list', auth, list);
+router.get('/find/:id', auth, find);
+router.put('/update/:id', auth, update);
+router.delete('/delete/:id', auth, remove);
 
 module.exports = router;
