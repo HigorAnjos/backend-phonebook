de9df976a4c27dc63d7ffc085d901e55295d5300
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-03 16:07:37 -0300

    feat(index): :sparkles: criado para exportar no test

---
+++ controller/phonebook/index.js
@@ -0,0 +1,13 @@
+const list = require('./list');
+const create = require('./create');
+const find = require('./find');
+const update = require('./update');
+const remove = require('./remove');
+
+module.exports ={
+  list,
+  create,
+  find,
+  update,
+  remove,
+}
