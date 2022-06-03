905b22c221bbfe93ebd8cb5e9183ce76da593f37
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-03 09:56:17 -0300

    refactor(update): :recycle: renomeando variaveis

--- models/phonebook/update.js
+++ models/phonebook/update.js
@@ -1,11 +1,11 @@
 const connection = require('../connection');
 
-const update = async (name, phone, use_id, phone_id) => {
-  const query = 'UPDATE model_phonebook.phonebook SET name = ?, phone = ? WHERE id = ? AND use_id = ?';
+const update = async (userId, name, phone, phoneId) => {
+  const query = 'UPDATE model_phonebook.phonebook SET name = ?, number = ? WHERE id = ? AND user_id = ?';
 
-  const [ phonebook ] = await connection.execute(query, [name, phone, phone_id, use_id]);
+  const [{ affectedRows }] = await connection.execute(query, [name, phone, phoneId, userId]);
 
-  return phonebook;
+  return affectedRows;
 }
 
 module.exports = update;
