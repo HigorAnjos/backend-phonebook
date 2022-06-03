7ac53a70e80a6729e7dbcc8545ab642ce7c69518
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-03 18:51:00 -0300

    style(list): :recycle: style

--- models/phonebook/list.js
+++ models/phonebook/list.js
@@ -6,12 +6,13 @@ const serialize = (phonebook, userId) => ({
 });
 
 const list = async (userId) => {
-const query = `SELECT phone.id, phone.name, phone.number as phone
-FROM model_phonebook.phonebook as phone
-INNER JOIN model_phonebook.user as user ON user.id = phone.user_id
-WHERE phone.user_id = ?`;
+  const query = `SELECT phone.id, phone.name, phone.number as phone
+  FROM model_phonebook.phonebook as phone
+  INNER JOIN model_phonebook.user as user ON user.id = phone.user_id
+  WHERE phone.user_id = ?`;
 
-const [phonebook] = await connection.execute(query, [userId]);
+  const [phonebook] = await connection.execute(query, [userId]);
+
   return serialize(phonebook, userId);
 };
 
