895f1ab3c5b9a72c2e48d2a0513b351c13c3b2d5
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-03 18:52:07 -0300

    test(models): :white_check_mark: create

---
+++ tests/models/phonebook/create.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../models/connection');
+const models = require('../../../models/phonebook/index');
+
+describe('Testando models create quando sucesso', () => {
+  const userId = 1;
+  const insertId = 5;
+  const name = 'Nome';
+  const phone = '+380967891234';
+  const DB_MOC = [{ insertId }];
+
+  before(() => {
+    sinon.stub(connection, 'execute').resolves(DB_MOC);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
+  it('Deve retornar um isertId', async () => {
+    const result = await models.create(name, phone, userId);
+    expect(result).to.be.equal(insertId);
+  });
+});
+
