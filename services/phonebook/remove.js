940ea35b690feb278cde2df6578f27fb8a851485
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-03 10:11:55 -0300

    feat(remove): :sparkles: servico de remover

---
+++ services/phonebook/remove.js
@@ -0,0 +1,9 @@
+const model = require('../../models/phonebook/index');
+
+const remove = async (userId, phoneId) => {
+  const affectedRows = await model.remove(userId, phoneId);
+
+  return affectedRows;
+}
+
+module.exports = remove;
