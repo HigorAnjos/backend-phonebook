9bcc1569a94d2ff1fe50691b4b6605c19d457e30
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-03 14:04:36 -0300

    feat(find): :sparkles: controller find
    encontrar contato

---
+++ controller/phonebook/find.js
@@ -0,0 +1,20 @@
+const services = require('../../services/phonebook/index');
+
+const find = async (req, res) => {
+  const { id: phoneId } = req.params;
+  const { id: userId } = req.user;
+  
+  const contactFound = await services.find(userId, phoneId);
+
+  if (!contactFound) {
+    return res.status(404).json({
+      message: 'Contato não encontrado',
+    });
+  }
+
+  console.log(contactFound);
+
+  return res.status(200).send(contactFound);
+}
+
+module.exports = find;
