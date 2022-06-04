ee22ec64a7fa7cfd4608123faf6bc2ad9586c2bb
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-04 01:13:42 -0300

    feat(find): :sparkles: encontrar user apenas por email

--- models/user/find.js
+++ models/user/find.js
@@ -1,9 +1,9 @@
 const connection = require('../connection');
 
-const find = async (email, password) => {
-  const query = 'SELECT * FROM model_phonebook.user WHERE email = ? AND password = ?';
+const find = async (email) => {
+  const query = 'SELECT * FROM model_phonebook.user WHERE email = ?';
 
-  const [user] = await connection.execute(query, [email, password]);
+  const [user] = await connection.execute(query, [email]);
 
   return user;
 };
