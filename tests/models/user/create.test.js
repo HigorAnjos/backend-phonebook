54b0b21ee6b2e02def2079ca95baeaa21ae8eae6
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-04 01:19:14 -0300

    test(models): :white_check_mark: create

---
+++ tests/models/user/create.test.js
@@ -0,0 +1,28 @@
+const models = require('../../../models/user');
+const sinon = require('sinon');
+const { expect } = require('chai');
+const connection = require('../../../models/connection');
+
+describe('Testando models create quando sucesso', () => {
+  const inserId = 1;
+  const DB_MOC = [{ inserId }];
+
+  before(() => {
+    sinon.stub(connection, 'execute').resolves(DB_MOC);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
+
+  it('Deve retornar inserId', async () => {
+    const name = 'George';
+    const email = 'george@gmail.com';
+    const password = '1234';
+
+    const newUserId = await models.create(name, email, password);
+
+    expect(newUserId).to.be.equal(inserId);
+  });
+});
