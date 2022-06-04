b7ee2aacb36f6b0a9d6d249ea23b7b6085f995c7
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-04 01:06:32 -0300

    feat(controller): :sparkles: bcrypt para poder guardar senha no banco

--- controller/user/create.js
+++ controller/user/create.js
@@ -1,4 +1,5 @@
 const services = require('../../services/user/index');
+const bcrypt = require('bcrypt');
 
 const create = async (req, res) => {
   const { name, email, password } = req.body;
@@ -7,15 +8,18 @@ const create = async (req, res) => {
       error: 'Dados incompletos',
     };
   }
-  const isOk = await services.create(name, email, password);
-  
+  // crypto password
+  const passwordHash = await bcrypt.hash(password, 10);
+  // create user
+  const isOk = await services.create(name, email, passwordHash);
+
   if (!isOk) {
     return res.status(400).json({
       error: 'Erro ao cria usuário',
     });
   }
 
-  return res.status(200).json(user);
+  return res.status(200).json(isOk);
 };
 
 module.exports = create;
