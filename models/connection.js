de8db84892b237cf5325402577a9446a45faf64e
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-06-05 18:53:13 -0300

    feat(router): :sparkles: test heroku

--- models/connection.js
+++ models/connection.js
@@ -2,11 +2,10 @@ const pgp = require('pg-promise')();
 require('dotenv').config();
 
 const db = pgp({
-  host: 'localhost' || process.env.DB_HOST,
-  port: 5432 || process.env.DB_PORT,
-  database: 'postgres' || process.env.DB_NAME,
-  user: 'postgres' || process.env.DB_USER,
-  password: 'postgres' || process.env.DB_PASS,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = db;
