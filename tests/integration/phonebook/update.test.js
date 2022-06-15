be724314360687e0c563584374dda97522d3699e
Higor Anjos <higorc.anjos@gmail.com>
2022-06-15 15:38:07 -0300

    docs(update): :white_check_mark: update integration

---
+++ tests/integration/phonebook/update.test.js
@@ -0,0 +1,45 @@
+const supertest = require("supertest");
+const shell = require('shelljs');
+const server = require("../../../server");
+const request = supertest(server);
+require("dotenv").config();
+
+const { sequelize: sequelizeCli } = require('../assets/constants');
+
+shell.exec([
+  sequelizeCli.drop,
+  sequelizeCli.create,
+  sequelizeCli.migrate,
+  sequelizeCli.seed
+].join(' && '),
+  { silent: process.env.DEBUG === "false" });
+
+describe.only('Rota Phonebook update', () => {
+
+  it('E possivel deletar um contato com sucesso', async () => {
+
+    const user = {
+      email: "george@gmail.com",
+      password: "1234",
+    };
+
+    const contactUpdate = {
+      name: 'higor anjos',
+      phone: '+5511985145589'
+    }
+
+    const { body: { token } } = await request.post('/user').send(user);
+
+    const response = await request.put('/phonebook/update/5').set('authorization', token).send(contactUpdate);
+
+    console.log('responde.body=>', response.body)
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('message');
+
+    const responseFind = await request.get('/phonebook/find/5').set('authorization', token);
+
+    expect(responseFind.status).toBe(200);
+    expect(responseFind.body.contact.name).toBe(contactUpdate.name);
+  });
+});
