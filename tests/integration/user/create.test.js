80840203403e117201167644f5a0f2e51846e04e
Higor Anjos <higorc.anjos@gmail.com>
2022-06-15 02:51:07 -0300

    test(create): :white_check_mark: validando rota user create

---
+++ tests/integration/user/create.test.js
@@ -0,0 +1,66 @@
+const supertest = require("supertest");
+const shell = require('shelljs');
+const server = require("../../../server");
+const request = supertest(server);
+require("dotenv").config();
+
+const { sequelize: sequelizeCli } = require('../assets/constants');
+
+shell.exec([
+  sequelizeCli.drop,
+  sequelizeCli.create,
+  sequelizeCli.migrate,
+  sequelizeCli.seed
+].join(' && '),
+  { silent: process.env.DEBUG === "false" });
+
+describe('Rota user create', () => {
+
+  it('E possivel criar um usuario com sucesso', async () => {
+    const newUser = {
+      name: 'higor anjos',
+      email: 'higorc.anjos@gmail.com',
+      password: '123456'
+    }
+
+    const response = await request.post('/user/create').send(newUser);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('Nao deve ser possivel criar um usuario sem email', async () => {
+    const newUser = {
+      name: 'higor anjos',
+      password: '123456'
+    }
+
+    const response = await request.post('/user/create').send(newUser);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('error');
+  });
+
+  it('Nao deve ser possivel criar um usuario sem nome', async () => {
+    const newUser = {
+      email: 'higorc.anjos@gmail.com',
+      password: '123456'
+    }
+    const response = await request.post('/user/create').send(newUser);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('error');
+  });
+
+  it('Nao deve ser possivel criar um usuario sem senha', async () => {
+    const newUser = {
+      name: 'higor anjos',
+      email: 'higorc.anjos@gmail.com',
+    }
+
+    const response = await request.post('/user/create').send(newUser);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('error');
+  });
+});
