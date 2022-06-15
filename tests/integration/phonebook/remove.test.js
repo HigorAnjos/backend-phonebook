6fa3a974df5987dd90ce8240d5097930a451ccaf
Higor Anjos <higorc.anjos@gmail.com>
2022-06-15 15:36:46 -0300

    test(remove): :white_check_mark: remove

---
+++ tests/integration/phonebook/remove.test.js
@@ -0,0 +1,39 @@
+const supertest = require("supertest");
+const shell = require('shelljs');
+const server = require("../../../server");
+const request = supertest(server);
+require("dotenv").config();
+
+const { sequelize: sequelizeCli } = require('../assets/constants');
+
+shell.exec([
+  sequelizeCli.drop,
+  sequelizeCli.create,
+  sequelizeCli.migrate,
+  sequelizeCli.seed
+].join(' && '),
+  { silent: process.env.DEBUG === "false" });
+
+describe('Rota Phonebook delete', () => {
+
+  it('E possivel deletar um contato com sucesso', async () => {
+
+    const user = {
+      email: "george@gmail.com",
+      password: "1234",
+    };
+
+    const { body: { token } } = await request.post('/user').send(user);
+
+    const response = await request.delete('/phonebook/delete/1').set('authorization', token);
+
+    console.log('Delete response.body =>', response.body);
+
+    expect(response.status).toBe(200);
+
+    const responseFind = await request.get('/phonebook/find/1').set('authorization', token);
+
+    expect(responseFind.status).toBe(404);
+    expect(responseFind.body).toHaveProperty('message');
+  });
+});
