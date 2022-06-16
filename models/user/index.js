858c70a2ea33cbc8268f42f647d525e58af8077e
Higor Anjos <higorc.anjos@gmail.com>
2022-06-16 09:46:07 -0300

    feat: :sparkles: linkando os arquivos

--- models/user/index.js
+++ models/user/index.js
@@ -1,4 +1,6 @@
 const find = require('./find');
 const create = require('./create');
+const remove = require('./remove');
+const update = require('./update');
 
-module.exports = { find, create };
+module.exports = { find, create, remove, update };
--- services/user/index.js
+++ services/user/index.js
@@ -1,5 +1,7 @@
 const create = require('./create');
 const find = require('./find');
 const login = require('./login');
+const update = require('./update');
+const remove = require('./remove');
 
-module.exports = { find, create, login };
+module.exports = { find, create, login, update, remove };
