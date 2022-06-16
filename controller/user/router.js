5ee6c4d91e63088fa47cf463beffc869dcde2605
Higor Anjos <higorc.anjos@gmail.com>
2022-06-16 09:44:47 -0300

    feat(router user): :sparkles: criando endpoits

--- controller/user/router.js
+++ controller/user/router.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { login, create } = require('./index');
+const auth = require('../../middlewares/auth');
+const { login, create, update, remove } = require('./index');
 
 const router = express.Router({ mergeParams: true });
 
 router.post('/', login);
 router.post('/create', create);
+router.put('/update', auth, update);
+router.delete('/delete', auth, remove);
 
 module.exports = router;
