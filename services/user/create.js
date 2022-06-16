bd885edbb3b9d457ffad43783cf33c5016248d2e
Higor Anjos <higorc.anjos@gmail.com>
2022-06-16 09:45:28 -0300

    feat(create): :sparkles: ao criar usuario o email precisa ser unico

--- services/user/create.js
+++ services/user/create.js
@@ -1,6 +1,13 @@
 const models = require('../../models/user');
+const find = require('./find');
 
 const create = async (name, email, password) => {
+  const userHasTheSameEmailFound = await find(email);
+
+  if (userHasTheSameEmailFound) {
+    return null; // email ja cadastrado
+  }
+
   const newUserId = await models.create(name, email, password);
 
   if (!newUserId) {
