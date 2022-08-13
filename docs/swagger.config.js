bdb7cfe291c2d52d235bfa04e0e81325ead42960
Higor Anjos <higorc.anjos@gmail.com>
2022-08-12 23:27:16 -0300

    feat: :sparkles: bearerAuth

--- docs/swagger.config.js
+++ docs/swagger.config.js
@@ -14,6 +14,15 @@ const swaggerConfig = {
         description: 'Servidor local',
       },
     ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
   },
   apis: ['./routes/*.js'],
 };
