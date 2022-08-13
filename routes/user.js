c46803eee99dd148411d6f277dce21a625cebdac
Higor Anjos <higorc.anjos@gmail.com>
2022-08-12 23:10:38 -0300

    docs: :memo: user login e create

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,79 @@ const { login, create, update, remove } = require('../controller/user');
 
 const router = express.Router({ mergeParams: true });
 
+/**
+ * @swagger
+ *  /user:
+ *   post:
+ *     tags: [User]
+ *     description: Login de usuario
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *              email: higorc.anjos@gmail.com
+ *              password: "123456"
+ *     responses:
+ *       200:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *               example:
+ *                  token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+ *
+ */
 router.post('/', login);
+
+/**
+ * @swagger
+ *  /user/create:
+ *   post:
+ *     tags: [User]
+ *     description: Criar um novo usuario
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *              name: higor anjos
+ *              email: higorc.anjos@gmail.com
+ *              password: "123456"
+ *     responses:
+ *       201:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: decimal
+ *               example:
+ *                  id: 5
+ *
+ */
 router.post('/create', create);
+
 router.put('/update', auth, update);
 router.delete('/delete', auth, remove);
 
