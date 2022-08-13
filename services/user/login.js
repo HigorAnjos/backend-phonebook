7846d44842bd6e0d1d561c0db9680f95623304b5
Higor Anjos <higorc.anjos@gmail.com>
2022-08-13 14:09:17 -0300

    refactor(find): :recycle: usar o find da models

--- services/user/login.js
+++ services/user/login.js
@@ -1,20 +1,19 @@
 const bcrypt = require('bcrypt');
-const find = require('./find');
+const models = require('../../models/user');
 
 const login = async (email, userInsertedPassword) => {
-  const userFound = await find(email); // humm pedir ajudar pra criar o teste ---
-  // console.log('userFound=> ', userFound);
+  const userFound = await models.find(email);
 
   if (!userFound) {
     return false;
   }
 
-  const isCorret = await bcrypt.compare(userInsertedPassword, userFound.password);
+  const isCorret = await bcrypt.compare(userInsertedPassword, userFound.dataValues.password);
   if (!isCorret) {
     return false;
   }
 
-  return userFound;
+  return userFound.dataValues;
 };
 
 module.exports = login;
