65406c94a73bdd76ba50223919d333d3b24f02da
Higor Anjos <higorc.anjos@gmail.com>
2022-08-13 11:18:14 -0300

    feat: :sparkles: ajustes para funcionar a docs

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,14 @@ require('dotenv').config();
 
 const API_SECRET = process.env.SECRET_KEY || 'mysecret';
 
+const withOutBearer = (token) => {
+  const auxToken = token;
+  if (auxToken.slice(0, 7) === 'Bearer ') {
+    return token.slice(7);
+  }
+  return token;
+};
+
 const auth = (req, res, next) => {
   try {
     const { authorization } = req.headers;
@@ -14,7 +22,7 @@ const auth = (req, res, next) => {
     }
 
     try {
-      const decoded = jwt.verify(authorization, API_SECRET);
+      const decoded = jwt.verify(withOutBearer(authorization), API_SECRET);
 
       const userName = decoded.data.name;
       const { id } = decoded.data;
