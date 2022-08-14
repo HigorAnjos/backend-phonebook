6e8dda5ca73c470124b9e9ab329a654af113ca16
Higor Anjos <higorc.anjos@gmail.com>
2022-08-14 11:00:30 -0300

    refactor: :recycle: utilizar o find da models

--- services/user/update.js
+++ services/user/update.js
@@ -1,14 +1,14 @@
 const models = require('../../models/user/index');
-const find = require('./find');
+// const find = require('./find');
 
 const update = async (name, email, password, userId) => {
-  const userHasTheSameEmailFound = await find(email);
+  const userHasTheSameEmailFound = await models.find(email);
 
-  if (userHasTheSameEmailFound && userId !== userHasTheSameEmailFound.id) {
+  if (userHasTheSameEmailFound && userId !== userHasTheSameEmailFound.dataValues.id) {
     return 0; // email ja cadastrado
   }
 
-  return models.update(name, email, password, userId);;
+  return models.update(name, email, password, userId);
 };
 
 module.exports = update;
